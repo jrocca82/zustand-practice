f100d291ee700e8aa89880a9db7b1244540201fa
Jo <loluvulol@gmail.com>
2023-06-14 09:43:20 +1000

    refactor: store logic

--- task-manager/src/components/Column.tsx
+++ task-manager/src/components/Column.tsx
@@ -1,22 +1,25 @@
-import { STATUSES, Statuses } from "../constants/statuses";
+import { Statuses } from "../constants/statuses";
 import { useTaskStore } from "../store";
 import "./Column.css";
 import Task from "./Task";
 
 const Column = ({ state }: { state: Statuses }) => {
-  const tasks = useTaskStore((store) => store.tasks);
+  const tasks = useTaskStore((store) =>
+    store.tasks.filter((task) => task.state === state)
+  );
 
   return (
     <div className="column">
-      <h3>{state}</h3>
+      <div className="header-wrapper">
+        <h3>{state}</h3>
+        {state === Statuses.PLANNED ? (
+          <button>+ Add</button>
+        ) : (
+          <div className="hidden" />
+        )}
+      </div>
       {tasks.map((task) => (
-        <>
-          {state === task.state ? (
-            <Task status={task.state} title={task.title} />
-          ) : (
-            <div className="hidden" />
-          )}
-        </>
+        <Task key={task.title} status={task.state} title={task.title} />
       ))}
     </div>
   );
--- task-manager/src/components/Task.tsx
+++ task-manager/src/components/Task.tsx
@@ -1,17 +1,24 @@
 import classNames from "classnames";
 import { Statuses } from "../constants/statuses";
+import { useTaskStore } from "../store";
 import "./Task.css";
 
-const Task = ({ title, status }: { title: string; status: Statuses }) => (
-  <div className="task-wrapper">
-    <div className="task">
-      <div className="checkbox" />
-      <p>{title}</p>
+const Task = ({ title, status }: { title: string; status: Statuses }) => {
+  const task = useTaskStore((store) =>
+    store.tasks.find((task) => task.state === status)
+  );
+
+  return (
+    <div className="task-wrapper">
+      <div className="task">
+        <div className={status === Statuses.ONGOING ? "checkbox" : "hidden"} />
+        <p>{task?.title}</p>
+      </div>
+      <div className="bottom-wrapper">
+        <div className={classNames("status", status)}>{task?.state}</div>
+      </div>
     </div>
-    <div className="bottom-wrapper">
-      <div className={classNames("status", status)}>{status}</div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Task;
